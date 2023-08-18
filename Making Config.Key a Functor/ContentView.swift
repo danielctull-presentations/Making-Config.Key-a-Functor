136fbf24dc7667adc4c9b8a2e2ab51b92c45f32d
Daniel Tull <dt@danieltull.co.uk>
2023-08-18 14:37:43 +0100

    Initial Commit

---
+++ Making Config.Key a Functor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Making Config.Key a Functor
+//
+//  Created by Tull, Daniel on 18/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Making Config.Key a Functor/Making_Config_Key_a_FunctorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Making_Config_Key_a_FunctorApp.swift
+//  Making Config.Key a Functor
+//
+//  Created by Tull, Daniel on 18/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Making_Config_Key_a_FunctorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
