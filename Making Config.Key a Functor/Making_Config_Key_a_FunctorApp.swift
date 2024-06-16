f34d19d422fd1ebe53df28e9e2c46a58c2f9a039
Daniel Tull <dt@danieltull.co.uk>
2024-06-16 20:18:07 +0100

    Update to the latest version of SlideUI

--- Making Config.Key a Functor/Making_Config_Key_a_FunctorApp.swift
+++ Making Config.Key a Functor/Making_Config_Key_a_FunctorApp.swift
@@ -1,17 +1,91 @@
-//
-//  Making_Config_Key_a_FunctorApp.swift
-//  Making Config.Key a Functor
-//
-//  Created by Tull, Daniel on 18/08/2023.
-//
-
+import SlideUI
 import SwiftUI
 
 @main
 struct Making_Config_Key_a_FunctorApp: App {
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        Presentation {
+
+            Slide(header: "Making Config.Key a Functor") { EmptyView() }
+                .slideStyle(.title)
+
+            Slide(header: "What is a Functor?") {
+                Text("A wrapper of a value that can be converted to a wrapper of a new value using a map function.")
+
+                HStack {
+                    Wrapper(
+                        name: "Wrapper",
+                        kind: "String",
+                        color: .red)
+
+                    Text("---->")
+
+                    Text("(String) -> Int")
+                        .padding()
+                        .background(.yellow, in: RoundedRectangle(cornerRadius: 4))
+
+                    Text("---->")
+
+                    Wrapper(
+                        name: "Wrapper",
+                        kind: "Int",
+                        color: .orange)
+                }
+            }
+
+            Slide(header: "Array is a Functor!") {
+
+                HStack {
+                    Wrapper(
+                        name: "Array",
+                        kind: "Product",
+                        color: .red)
+
+                    Text("---->")
+
+                    VStack {
+                        Code { "map" }
+                        Text("(Product) -> Product.ID")
+                            .padding()
+                            .background(.yellow, in: RoundedRectangle(cornerRadius: 4))
+                    }
+
+                    Text("---->")
+
+                    Wrapper(
+                        name: "Array",
+                        kind: "Product.ID",
+                        color: .orange)
+                }
+            }
+
+            Slide(header: "Examples of Functors in Swift") {
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("• Array")
+                    Text("• Optional")
+                    Text("• String")
+                    Text("• Dictionary").strikethrough()
+                }
+            }
         }
+        .slideStyle(.content)
+    }
+}
+
+struct Wrapper: View {
+    let name: String
+    let kind: String
+    let color: Color
+
+    var body: some View {
+        VStack {
+            Text(name)
+
+            Text(kind)
+                .padding()
+                .border(color, width: 4)
+        }
+        .padding()
+        .border(.black, width: 4)
     }
 }
